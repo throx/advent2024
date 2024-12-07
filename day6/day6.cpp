d1d9b522fdc0601fa33e8a4eab8446b221adae2f
John Wiltshire <jw@chase.net.au>
2024-12-07 14:53:35 +1000

    Properly optimised day 6

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -23,10 +23,8 @@ struct std::hash<pair<Point2, Point2>>
     }
 };
 
-int main()
+void original()
 {
-    auto time_start = std::chrono::high_resolution_clock::now();
-
     set<Point2> obs;
     set<Point2> been;
     int maxx = 0;
@@ -86,6 +84,7 @@ int main()
                 guard.first = next;
                 if (looptrack.contains(guard)) {
                     // In a loop
+                    cout << "#" << part2 << ": " << block[0] << ", " << block[1] << endl;
                     ++part2;
                     break;
                 }
@@ -96,9 +95,110 @@ int main()
 
     cout << "Part 1: " << part1 << endl;
     cout << "Part 2: " << part2 << endl;
+}
 
+void faster()
+{
+    static const int SIZE = 256;
+    bool obs[SIZE][SIZE];
+    int maxx = 0;
+    int maxy = 0;
+    int start_x;
+    int start_y;
+
+    memset(obs, 0, sizeof(obs));
+    string s;
+    while (getline(cin, s)) {
+        maxx = (int)s.length();
+        for (int x = 0; x < maxx; ++x) {
+            if (s[x] == '#') {
+                obs[x][maxy] = true;
+            }
+            if (s[x] == '^') {
+                start_x = x;
+                start_y = maxy;
+            }
+        }
+        ++maxy;
+    }
+
+    int x = start_x;
+    int y = start_y;
+    int dir = 0;
+    const int DIRX[] = { 0, 1, 0, -1 };
+    const int DIRY[] = { -1, 0, 1, 0 };
+
+    bool been[SIZE][SIZE];
+    memset(been, 0, sizeof(been));
+
+    while (x >= 0 && y >= 0 && x < maxx && y < maxy) {
+        been[x][y] = true;
+        int next_x = x + DIRX[dir];
+        int next_y = y + DIRY[dir];
+        if (next_x < 0 || next_y < 0 || next_x >= maxx || next_y >= maxy) break;
+        if (obs[next_x][next_y]) {
+            dir = (dir + 1) & 0x3;
+        }
+        else {
+            x = next_x;
+            y = next_y;
+        }
+    }
+
+    int part1 = 0;
+    int part2 = 0;
+
+    for (int block_x = 0; block_x < maxx; ++block_x) {
+        for (int block_y = 0; block_y < maxy; ++block_y) {
+            if (been[block_x][block_y]) {
+                ++part1;
+
+                if (block_x == start_x && block_y == start_y) continue;
+
+                obs[block_x][block_y] = true;
+
+                x = start_x;
+                y = start_y;
+                dir = 0;
+                bool been2[4][SIZE][SIZE];
+                memset(been2, 0, sizeof(been2));
+
+                while (x >= 0 && y >= 0 && x < maxx && y < maxy) {
+                    if (been2[dir][x][y]) {
+                        ++part2;
+                        break;
+                    }
+                    been2[dir][x][y] = true;
+                    int next_x = x + DIRX[dir];
+                    int next_y = y + DIRY[dir];
+                    if (next_x < 0 || next_y < 0 || next_x >= maxx || next_y >= maxy) break;
+                    if (obs[next_x][next_y]) {
+                        dir = (dir + 1) & 0x3;
+                    }
+                    else {
+                        x = next_x;
+                        y = next_y;
+                    }
+                }
+
+                obs[block_x][block_y] = false;
+            }
+        }
+    }
+
+
+    cout << "Part 1: " << part1 << endl;
+    cout << "Part 2: " << part2 << endl;
+}
+
+int main()
+{
+    auto time_start = std::chrono::high_resolution_clock::now();
+    //original();
+    faster();
     auto time_finish = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(time_finish - time_start).count() << "ns\n";
+
+    std::cout << "Time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(time_finish - time_start).count() << "ns\n";
 
     cout << "Press a key to continue" << endl;
     _getch();
