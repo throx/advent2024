54e53a757306099ce4775d980acb5b412c38f693
John Wiltshire <jw@chase.net.au>
2024-12-20 18:41:28 +1000

    Day 20 done

--- day20/day20.cpp
+++ day20/day20.cpp
@@ -14,19 +14,16 @@ static inline void _getch() {}
 
 using namespace std;
 
-int race(const Point2& start, const Point2& end, const set<Point2>& walls, const Rect2& bounds)
+map<Point2, int> paint(const Point2& start, const Point2& end, const set<Point2>& walls, const Rect2& bounds)
 {
     set<Point2> todo({ start });
-    set<Point2> done;
+    map<Point2, int> done;
     int steps = 0;
-    while (true) {
+    while (!todo.empty()) {
         set<Point2> next;
         for (const auto& p : todo) {
-            if (p == end) {
-                goto done;
-            }
             if (!done.contains(p)) {
-                done.insert(p);
+                done[p] = steps;
                 p.DoSquareNeighbours([&](const auto& n)
                     {
                         if (bounds.Contains(n) && !walls.contains(n)) {
@@ -39,8 +36,32 @@ int race(const Point2& start, const Point2& end, const set<Point2>& walls, const
         todo.swap(next);
     }
 
-done:
-    return steps;
+    return done;
+}
+
+map<int, int> count_ways(const map<Point2, int>& start_paint, const map<Point2, int>& end_paint, int max_cheat, int nocheat)
+{
+    map<int, int> tally;
+    for (const auto& start_entry : start_paint) {
+        const auto& s = start_entry.first;
+        for (int dx = -max_cheat; dx <= max_cheat; ++dx) {
+            int d = max_cheat - abs(dx);
+            for (int dy = -d; dy <= d; ++dy) {
+                int dist = abs(dx) + abs(dy);
+                if (dist == 0) continue;
+                Point2 e = s + (P2::S * dy) + (P2::E * dx);
+                auto e_it = end_paint.find(e);
+                if (e_it != end_paint.end()) {
+                    int cost = e_it->second + start_entry.second + dist;
+                    if (cost < nocheat) {
+                        tally[nocheat - cost]++;
+                    }
+                }
+            }
+        }
+    }
+
+    return tally;
 }
 
 int main()
@@ -73,38 +94,33 @@ int main()
 
     Rect2 bounds({ 0, 0 }, { maxx, y });
 
-    auto nocheat = race(start, end, walls, bounds);
-    map<int, int> tally;
-
-    auto walls_copy = walls;
-    int c = 0;
-    for (auto it = walls.begin(); it != walls.end(); ++it) {
-        cout << ++c << " of " << walls.size() << endl;
-        int sum_walls = 0;
-        it->DoSquareNeighbours([&](const auto& n)
-            {
-                if (walls.contains(n)) {
-                    ++sum_walls;
-                }
-            });
-        if (sum_walls <= 2) {
-            walls_copy.erase(*it);
-            auto cheat = race(start, end, walls_copy, bounds);
-            if (cheat < nocheat) {
-                ++tally[nocheat - cheat];
-            }
-            walls_copy.insert(*it);
-        }
-    }
+    // Strategy is determine the distance to every point from the start and from the end.
+    // A cheat is valid if the manhattan distance between two points is less than the
+    // allowed cheat, and adding the distances with the manhattan distance gives less than
+    // the original distance.
+    auto start_paint = paint(start, end, walls, bounds);
+    auto end_paint = paint(end, start, walls, bounds);
+    auto nocheat = start_paint.at(end);
 
     auto part1 = 0LL;
     auto part2 = 0LL;
+
+    auto tally = count_ways(start_paint, end_paint, 2, nocheat);
 
     for (const auto& e : tally) {
         if (e.first >= 100) {
             part1 += e.second;
         }
-        cout << e.first << ": " << e.second << endl;
+        //cout << e.first << ": " << e.second << endl;
+    }
+
+    tally = count_ways(start_paint, end_paint, 20, nocheat);
+
+    for (const auto& e : tally) {
+        if (e.first >= 100) {
+            part2 += e.second;
+        }
+        //cout << e.first << ": " << e.second << endl;
     }
 
 
